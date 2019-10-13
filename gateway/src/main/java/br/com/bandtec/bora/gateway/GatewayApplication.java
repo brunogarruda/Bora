08e09640d0a4487b3bd25d816a7bb89a1eece0c6
GBernardo10 <g_bernardo10@hotmail.com>
2019-10-13 00:32:44 -0300

    gateway

--- gateway/src/main/java/br/com/bandtec/bora/gateway/GatewayApplication.java
+++ gateway/src/main/java/br/com/bandtec/bora/gateway/GatewayApplication.java
@@ -1,13 +1,18 @@
 package br.com.bandtec.bora.gateway;
 
+import br.com.bandtec.bora.core.property.JwtConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableZuulProxy
 @EnableEurekaClient
+@EnableConfigurationProperties(value = JwtConfiguration.class)
+@ComponentScan("br.com.bandtec.bora")
 public class GatewayApplication {
 
     public static void main(String[] args) {
---
+++ gateway/src/main/java/br/com/bandtec/bora/gateway/security/config/SecurityConfig.java
@@ -0,0 +1,9 @@
+package br.com.bandtec.bora.gateway.security.config;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import br.com.bandtec.bora.token.security.config.SecurityTokenConfig;
+
+@EnableWebSecurity
+public class SecurityConfig extends SecurityT {
+
+}
