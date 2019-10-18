56b0625cd0f1a3097b9e9d886a51b9a6d7309708
GBernardo10 <g_bernardo10@hotmail.com>
2019-10-17 23:38:19 -0300

    eureka

--- eventos/src/main/java/br/com/bandtec/bora/BoraApplication.java
+++ eventos/src/main/java/br/com/bandtec/bora/BoraApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import br.com.bandtec.bora.core.property.JwtConfiguration;
@@ -13,6 +14,7 @@ import br.com.bandtec.bora.core.property.JwtConfiguration;
 @EnableJpaRepositories(basePackages = { "br.com.bandtec.bora.core.repository" })
 @EnableConfigurationProperties(value = JwtConfiguration.class)
 @ComponentScan(basePackages = { "br.com.bandtec.bora" })
+@EnableEurekaClient
 public class BoraApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BoraApplication.class, args);
