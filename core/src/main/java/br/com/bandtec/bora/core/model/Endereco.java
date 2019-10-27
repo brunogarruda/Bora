c07d1badfc564239e2c5413b4d76e3b15ae14a7a
GBernardo10 <g_bernardo10@hotmail.com>
2019-10-27 00:07:55 -0300

    adiciona sub categoria e endereco

---
+++ core/src/main/java/br/com/bandtec/bora/core/model/Endereco.java
@@ -0,0 +1,46 @@
+package br.com.bandtec.bora.core.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Builder
+@Data
+@Entity
+@Table(name = "tbd_endereco")
+public class Endereco {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_endereco")
+    private Long id;
+
+    @Column(name = "cod_endereco")
+    private String codigoEndereco;
+
+    private String rua;
+
+    private String numero;
+
+    private String cep;
+
+    private String bairro;
+
+    private String cidade;
+
+    private String estado;
+
+}
