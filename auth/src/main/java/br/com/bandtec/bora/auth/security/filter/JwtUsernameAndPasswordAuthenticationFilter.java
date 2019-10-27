e55dcac764b2c4164a66e58eb826925ad16285da
GBernardo10 <g_bernardo10@hotmail.com>
2019-10-27 06:15:51 -0300

    up

--- auth/src/main/java/br/com/bandtec/bora/auth/security/filter/JwtUsernameAndPasswordAuthenticationFilter.java
+++ auth/src/main/java/br/com/bandtec/bora/auth/security/filter/JwtUsernameAndPasswordAuthenticationFilter.java
@@ -1,8 +1,9 @@
 package br.com.bandtec.bora.auth.security.filter;
 
-import br.com.bandtec.bora.core.property.JwtConfiguration;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import br.com.bandtec.bora.core.model.Usuario;
+import br.com.bandtec.bora.core.property.JwtConfiguration;
+import br.com.bandtec.bora.token.security.token.TokenCreator;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nimbusds.jwt.SignedJWT;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
@@ -14,7 +15,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import br.com.bandtec.bora.token.security.token.TokenCreator;
 import javax.servlet.FilterChain;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -27,10 +27,10 @@ public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePassword
 	private final JwtConfiguration jwtConfiguration;
 	private final TokenCreator tokenCreator;
 
-	@Override
-	@SneakyThrows
-	public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
-			throws AuthenticationException {
+  @Override
+  @SneakyThrows
+    public Authentication attemptAuthentication(
+        HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
 		log.info("Autenticando...");
 		Usuario usuario = new ObjectMapper().readValue(request.getInputStream(), Usuario.class);
 
@@ -53,9 +53,9 @@ public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePassword
 	@SneakyThrows
 	protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
 			Authentication auth) {
-		log.info("Authentication was successful for the user '{}', generating JWE token", auth.getName());
+        log.info("Authentication was successful for the user '{}', generating JWE token", auth.getName());
 
-		SignedJWT signedJWT = tokenCreator.createSignedJWT(auth);
+        SignedJWT signedJWT = tokenCreator.createSignedJWT(auth);
 
 		String encryptedToken = tokenCreator.encryptToken(signedJWT);
 
