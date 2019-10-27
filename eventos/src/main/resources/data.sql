8fae17a285119a084a5e215915aaf8b3a7e4c596
GBernardo10 <g_bernardo10@hotmail.com>
2019-10-26 22:47:07 -0300

    remove bin

--- eventos/src/main/resources/data.sql
+++ eventos/src/main/resources/data.sql
@@ -1,5 +1,5 @@
 INSERT INTO tbd_usuario
-    ([apelido],[senha],[role])
+    (apelido,senha,role)
 VALUES('Medina', 'LFX24XZK7SI', 'user'),
     ('Dean', 'QJX93JSF2VH', 'user'),
     ('Morales', 'NSG65FON3XN', 'user'),
@@ -11,7 +11,7 @@ VALUES('Medina', 'LFX24XZK7SI', 'user'),
     ('Justice', 'YKW11AFB9AG', 'user'),
     ('Holder', 'ATT63UDS8BM', 'user');
 INSERT INTO tbd_usuario
-    ([apelido],[senha],[role])
+    (apelido,senha,role)
 VALUES('Davidson', 'DSB71QLL3YB', 'user'),
     ('Mcneil', 'TRJ65KOT5MN', 'user'),
     ('Carroll', 'JFD02HKB1KD', 'user'),
@@ -23,7 +23,7 @@ VALUES('Davidson', 'DSB71QLL3YB', 'user'),
     ('Meyers', 'WHX90NLB5IR', 'user'),
     ('Copeland', 'SOX37FYM0NH', 'user');
 INSERT INTO tbd_usuario
-    ([apelido],[senha],[role])
+    (apelido,senha,role)
 VALUES('Flowers', 'MUU90YDN4CN', 'user'),
     ('Clements', 'NFK38NCQ9VB', 'user'),
     ('Buckley', 'AYC03FVM6LL', 'user'),
@@ -35,7 +35,7 @@ VALUES('Flowers', 'MUU90YDN4CN', 'user'),
     ('Flores', 'EYD58QTB0AK', 'user'),
     ('Vargas', 'BGH51JVO2SD', 'user');
 INSERT INTO tbd_usuario
-    ([apelido],[senha],[role])
+    (apelido,senha,role)
 VALUES('Edwards', 'GCD06NOI7JX', 'user'),
     ('Guy', 'LGQ78LAN4MP', 'user'),
     ('Montgomery', 'QTA69RMN0JH', 'user'),
@@ -47,7 +47,7 @@ VALUES('Edwards', 'GCD06NOI7JX', 'user'),
     ('Jordan', 'UUJ89BIM9QL', 'user'),
     ('Ochoa', 'AIE09GOK7WM', 'user');
 INSERT INTO tbd_usuario
-    ([apelido],[senha],[role])
+    (apelido,senha,role)
 VALUES('Harmon', 'YJB02NHV7JK', 'user'),
     ('Bender', 'OTN21BIW3ZB', 'user'),
     ('Meyers', 'XCY71HDU4IK', 'user'),
@@ -59,7 +59,7 @@ VALUES('Harmon', 'YJB02NHV7JK', 'user'),
     ('Puckett', 'GEO17IDK5ZF', 'user'),
     ('Flores', 'JEY33XGH0PU', 'user');
 INSERT INTO tbd_usuario
-    ([apelido],[senha],[role])
+    (apelido,senha,role)
 VALUES('Reeves', 'RWH04XCG5QC', 'user'),
     ('Kelly', 'HJU76YCM1XB', 'user'),
     ('Walton', 'EIZ25BLY2HV', 'user'),
@@ -71,7 +71,7 @@ VALUES('Reeves', 'RWH04XCG5QC', 'user'),
     ('Calderon', 'BYP80NLN4AL', 'user'),
     ('Rosales', 'BOQ84HWF8GB', 'user');
 INSERT INTO tbd_usuario
-    ([apelido],[senha],[role])
+    (apelido,senha,role)
 VALUES('Trujillo', 'EGX83ZLQ6HS', 'user'),
     ('Black', 'VKU23OAM5BD', 'user'),
     ('Whitfield', 'VFV54SRY7ET', 'user'),
@@ -83,7 +83,7 @@ VALUES('Trujillo', 'EGX83ZLQ6HS', 'user'),
     ('Calhoun', 'JUB02LLZ5UI', 'user'),
     ('Greer', 'QGN64MAI8SS', 'user');
 INSERT INTO tbd_usuario
-    ([apelido],[senha],[role])
+    (apelido,senha,role)
 VALUES('Combs', 'EIK13DUH7EB', 'user'),
     ('Scott', 'RNO48ZDW5LH', 'user'),
     ('Schwartz', 'AJV98UAO6WU', 'user'),
@@ -95,7 +95,7 @@ VALUES('Combs', 'EIK13DUH7EB', 'user'),
     ('Delgado', 'GLV79FEX0AA', 'user'),
     ('Joyce', 'YBB04AXT6II', 'user');
 INSERT INTO tbd_usuario
-    ([apelido],[senha],[role])
+    (apelido,senha,role)
 VALUES('Franklin', 'FBU23INJ5GV', 'user'),
     ('Nixon', 'DKZ40PQS9BS', 'user'),
     ('Salazar', 'ASQ48TCJ6QE', 'user'),
@@ -107,7 +107,7 @@ VALUES('Franklin', 'FBU23INJ5GV', 'user'),
     ('Barr', 'KPT24ZHC2MG', 'user'),
     ('Velasquez', 'TEM25SZB7YL', 'user');
 INSERT INTO tbd_usuario
-    ([apelido],[senha],[role])
+    (apelido,senha,role)
 VALUES('Holman', 'SWX86ULP9ER', 'user'),
     ('King', 'IKY94TGT3BW', 'user'),
     ('Potter', 'YUQ78XFV0HP', 'user'),
@@ -118,3 +118,134 @@ VALUES('Holman', 'SWX86ULP9ER', 'user'),
     ('English', 'UJO84BOT0ZR', 'user'),
     ('Hutchinson', 'AER49ATT8FR', 'user'),
     ('Sellers', 'NLS96VOL6FA', 'user');
+
+INSERT INTO tbd_evento
+    (nome)
+VALUES
+    ('Reynolds'),
+    ('Gardner'),
+    ('Shaw'),
+    ('Baird'),
+    ('Figueroa'),
+    ('Mcmillan'),
+    ('Page'),
+    ('Mcintyre'),
+    ('Mays'),
+    ('Bowman');
+INSERT INTO tbd_evento
+    (nome)
+VALUES
+    ('Mosley'),
+    ('Stevens'),
+    ('Russell'),
+    ('Sparks'),
+    ('Wooten'),
+    ('Bernard'),
+    ('Hodges'),
+    ('Dudley'),
+    ('Rodriquez'),
+    ('Sears');
+INSERT INTO tbd_evento
+    (nome)
+VALUES
+    ('Leach'),
+    ('Gray'),
+    ('Wolf'),
+    ('Gentry'),
+    ('Skinner'),
+    ('Horne'),
+    ('Levy'),
+    ('Wells'),
+    ('Hensley'),
+    ('Dillard');
+INSERT INTO tbd_evento
+    (nome)
+VALUES
+    ('Sykes'),
+    ('Burgess'),
+    ('Mckenzie'),
+    ('Sykes'),
+    ('Hartman'),
+    ('Koch'),
+    ('Bowers'),
+    ('Casey'),
+    ('Strickland'),
+    ('Tillman');
+INSERT INTO tbd_evento
+    (nome)
+VALUES
+    ('Mcguire'),
+    ('Valentine'),
+    ('Mosley'),
+    ('Johnston'),
+    ('Foreman'),
+    ('Brewer'),
+    ('Castillo'),
+    ('Montoya'),
+    ('Coleman'),
+    ('Dillon');
+INSERT INTO tbd_evento
+    (nome)
+VALUES
+    ('Tyson'),
+    ('Ray'),
+    ('Bernard'),
+    ('Chen'),
+    ('Marsh'),
+    ('Wynn'),
+    ('Jensen'),
+    ('Mayo'),
+    ('Levy'),
+    ('Bright');
+INSERT INTO tbd_evento
+    (nome)
+VALUES
+    ('Lee'),
+    ('Snow'),
+    ('Sparks'),
+    ('Singleton'),
+    ('Dalton'),
+    ('Watson'),
+    ('Pitts'),
+    ('Simmons'),
+    ('Manning'),
+    ('Holland');
+INSERT INTO tbd_evento
+    (nome)
+VALUES
+    ('Simpson'),
+    ('Sullivan'),
+    ('Sykes'),
+    ('Parker'),
+    ('Gillespie'),
+    ('David'),
+    ('Douglas'),
+    ('Sullivan'),
+    ('Kent'),
+    ('Rice');
+INSERT INTO tbd_evento
+    (nome)
+VALUES
+    ('Burt'),
+    ('Mcknight'),
+    ('Armstrong'),
+    ('Bauer'),
+    ('Cross'),
+    ('Trevino'),
+    ('Swanson'),
+    ('Boyer'),
+    ('Marshall'),
+    ('Stevens');
+INSERT INTO tbd_evento
+    (nome)
+VALUES
+    ('Boyer'),
+    ('Aguilar'),
+    ('Marquez'),
+    ('Simmons'),
+    ('Lawson'),
+    ('Waller'),
+    ('Cox'),
+    ('Travis'),
+    ('Goodman'),
+    ('Schmidt');
